cf613e6190316528ec18c6f542dcfa80bcb2c74f
topwasu <wasu.piri@gmail.com>
2023-10-17 11:24:50 -0400

    Don't import hf

--- openai_hf_interface/utils.py
+++ openai_hf_interface/utils.py
@@ -1,5 +1,4 @@
 from .formatter import DoNothingFormatter, LLaMaChatFormatter, OpenAIChatFormatter
-from .hf import HF_LLM, LLaMA_model
 from .openai import OpenAI_LLM, prompt_openai_single, prompt_openai_chat_single
 
 
@@ -19,6 +18,7 @@ def create_llm(model_name):
     elif model_name.startswith('gpt-3.5') or model_name.startswith('gpt-4'):
         return OpenAI_LLM(model_name, prompt_openai_chat_single, formatter)
     elif model_name.startswith('meta-llama'):
+        from .hf import HF_LLM, LLaMA_model
         return HF_LLM(model_name, LLaMA_model(model_name), formatter)
     else:
         raise NotImplementedError
